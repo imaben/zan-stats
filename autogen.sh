b755ec2f437c3a74eab127350373f39f47ffea0b
maben <www.maben@foxmail.com>
2017-08-13 17:21:28 +0800

    add autogen

---
+++ autogen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "x$(uname)" = "xOpenBSD" ]; then
+        [ -z "$AUTOMAKE_VERSION" ] && export AUTOMAKE_VERSION=1.10
+        [ -z "$AUTOCONF_VERSION" ] && export AUTOCONF_VERSION=2.65
+fi
+
+die()
+{
+    echo "$@" >&2
+    exit 1
+}
+
+mkdir -p etc
+aclocal || die "aclocal failed"
+autoconf || die "autoconf failed"
+autoheader || die "autoheader failed"
+automake --add-missing --force-missing --copy --foreign || die "automake failed"
+autoreconf || die "autoreconf failed"
