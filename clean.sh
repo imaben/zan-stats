d7deb31203ec9f3857458bd7ed3c6d76ca8bc307
maben <www.maben@foxmail.com>
2017-08-30 11:26:18 +0800

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+rm -rf .deps/
+rm -rf Makefile
+rm -rf Makefile.in
+rm -rf aclocal.m4
+rm -rf autom4te.cache/
+rm -rf compile
+rm -rf config.h
+rm -rf config.h.in
+rm -rf config.log
+rm -rf config.status
+rm -rf configure
+rm -rf depcomp
+rm -rf install-sh
+rm -rf missing
+rm -rf stamp-h1
